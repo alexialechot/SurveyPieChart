28b08c10d9000fc1705952e1443b61fb899e804f
Alex lechot <admin@uxmilk.ch>
2019-01-23 11:18:37 +0100

    Construction Scenes

---
+++ Survey/Assets/Script/Construction/PieChart.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PieChart : MonoBehaviour
+{
+
+    public Text totalText;
+    public int total = 0;
+
+    public Text textTotalOption0;
+    public Text textTotalOption1;
+    public Text textTotalOption2;
+
+    public int totalOption0 = 0;
+    public int totalOption1 = 0;
+    public int totalOption2 = 0;
+
+    public Text textPercentage0;
+    public Text textPercentage1;
+    public Text textPercentage2;
+
+    public float Percentage0 = 0F;
+    public float Percentage1 = 0F;
+    public float Percentage2 = 0F;
+
+    public float[] options;
+    public Color[] PieColors;
+    public Image PiePrefab;
+
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ START ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    void Start()
+    {
+        //-------------------------------------------- color ---------------------------------------------------
+        textPercentage0.color = PieColors[0];
+        textPercentage1.color = PieColors[1];
+        textPercentage2.color = PieColors[2];
+        //----------------------------------------- make the PIE -----------------------------------------------
+        if (total != 0)
+        {
+            Pie();
+        }
+        //----------------------------------------- Set the TEXT -----------------------------------------------
+        textTotalOption0.text = totalOption0.ToString();
+        textTotalOption1.text = totalOption1.ToString();
+        textTotalOption2.text = totalOption2.ToString();
+        totalText.text = total.ToString();
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //----------------------------------------------- UPDATE ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    void Update()
+    {
+        if (total != 0)
+        {
+            Percentage0 = 100 * totalOption0 / total;
+            textPercentage0.text = Percentage0.ToString() + " %";
+
+            Percentage1 = 100 * totalOption1 / total;
+            textPercentage1.text = Percentage1.ToString() + " %";
+
+            Percentage2 = 100 * totalOption2 / total;
+            textPercentage2.text = Percentage2.ToString() + " %";
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //-------------------------------------------- VisitorNumber -----------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    public void VisitorNumber()
+    {
+        total++;
+        totalText.text = total.ToString();
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //----------------------------------------------- OPTIONS --------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    // Call Option with Buttons
+    public void Option0()
+    {
+        totalOption0++;
+        textTotalOption0.text = totalOption0.ToString();
+        options[0] = totalOption0;
+        Pie();
+    }
+    public void Option1()
+    {
+        totalOption1++;
+        textTotalOption1.text = totalOption1.ToString();
+        options[1] = totalOption1;
+        Pie();
+    }
+    public void Option2()
+    {
+        totalOption2++;
+        textTotalOption2.text = totalOption2.ToString();
+        options[2] = totalOption2;
+        Pie();
+    }
+
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------- PIE ----------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+	void Pie()
+    {
+        float zRotation = 0F;
+        float totalPie = 0F;
+
+        for (int i = 0; i < options.Length; i++)
+        {
+            totalPie += options[i];
+        }
+        for (int i = 0; i < options.Length; i++)
+        {
+            Image newPie = Instantiate(PiePrefab) as Image;
+            newPie.transform.SetParent(transform, false);
+            newPie.color = PieColors[i];
+            newPie.fillAmount = options[i] / totalPie;
+            newPie.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, zRotation));
+            zRotation -= newPie.fillAmount * 360f;
+        }
+    }
+}
---
+++ Survey/Assets/Script/PieChartToggle.cs
@@ -0,0 +1,253 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PieChartToggle : MonoBehaviour
+{
+    public int OptionsTotal;
+
+    public Text totalText;
+    public int total = 0;
+    public Text totalVisiteursText;
+    public int totalVisiteurs = 0;
+
+    public Toggle[] toggleOptions;
+    public Image[] toggleBackgrounds;
+    public Image[] toggleCheckmarks;
+    public Image[] toggleCheckmarksDeco;
+
+    public Text[] textTotalOptions;
+    public int[] totalOptions;
+
+    public Text[] textPercentages;
+    public float[] Percentages;
+
+    public Button buttonEnter;
+
+    public float[] options; // Pie need a float ... but exactly the same as totalOptions
+    public Color[] PieColors;
+    public Image PiePrefab;
+
+    float timeLeft;
+    public float TheTimeLeft = 60.0f;
+    bool timeOut = false;
+    float thanksTimeLeft;
+    public float theThanksTimeLeft = 3.0f;
+    bool thanksTime = false;
+    public Image thanks;
+
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ START ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+	void Start()
+    {
+        //--------------------------------------------- COLORS --------------------------------------------------
+        // set the colors of the text
+        for (int i = 0; i < textPercentages.Length; i++)
+        {
+            textPercentages[i].color = PieColors[i];
+        }
+        //set the colors of the button
+        for (int i = 0; i < toggleBackgrounds.Length; i++)
+        {
+            toggleBackgrounds[i].color = PieColors[i];
+        }
+        //set the colors of the button checkmark
+        for (int i = 0; i < toggleCheckmarks.Length; i++)
+        {
+            toggleCheckmarks[i].color = PieColors[i];
+        }
+        for (int i = 0; i < toggleOptions.Length; i++)
+        {
+            textTotalOptions[i].text = totalOptions[i].ToString();
+        }
+
+        //------------------------------------------------ Set the TEXT ---------------------------------------------------
+        totalVisiteursText.text = totalVisiteurs.ToString();
+        totalText.text = total.ToString();
+        //------------------------------------------------ make the PIE ---------------------------------------------------
+        if (totalVisiteurs != 0)
+        {
+            Pie();
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ UPDATE ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    void Update()
+    {
+        //------------------------------------------------ PERCENTAGES ---------------------------------------------------
+        // make the percentages
+        if (total != 0)
+        {
+            for (int i = 0; i < totalOptions.Length; i++)
+            {
+                Percentages[i] = 100 * totalOptions[i] / total;
+                textPercentages[i].text = Percentages[i].ToString() + " %";
+            }
+        }
+        //------------------------------------------------ TIMES ---------------------------------------------------
+        // Time before reset
+        if (timeOut == true)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft <= 0.0f)
+            {
+                Debug.Log("time is left");
+                Reset();
+                timeOut = false;
+            }
+        }
+        // Time of thanks page stay
+        if (thanksTime == true)
+        {
+            thanksTimeLeft -= Time.deltaTime;
+            if (thanksTimeLeft <= 0.0f)
+            {
+                //Debug.Log("thanksTime is left");
+                thanks.GetComponent<Image>().enabled = false;
+                thanksTime = false;
+            }
+        }
+        //------------------------------------------------ DECORATION ON CLICK ---------------------------------------------------
+        // call checkmarks deco when a ToggleOption isOn
+        // Interactable buttonEnter when a ToggleOption isOn
+        // Change color of buttonEnter
+
+
+        for (int i = 0; i < toggleOptions.Length; i++)
+            {
+                if (toggleOptions[i].isOn)
+                {
+                    toggleCheckmarksDeco[i].GetComponent<Image>().enabled = true;
+                    buttonEnter.GetComponent<Button>().enabled = true;
+                    //ColorBlock colors = buttonEnter.colors;
+                    //colors.normalColor = new Color32(255, 0, 0, 255);
+                    //buttonEnter.colors = colors;
+                }
+               else
+               {
+                   toggleCheckmarksDeco[i].GetComponent<Image>().enabled = false;
+                }
+           }
+
+
+        //------------------------------------------------ VALIDATION ENABLED ---------------------------------------------------
+        //Check if one of the options is selected
+        bool allTogglesOff = true;
+        for (int i = 0; i < toggleOptions.Length; ++i)
+        {
+            if (toggleOptions[i].isOn){
+                allTogglesOff = false;
+                break;
+            }
+        }
+        // if it's not the case (no ones is selected)
+        if (allTogglesOff)
+        {
+            //Debug.Log("All the Toggle are false");
+            // the validation button is not enabled
+            buttonEnter.GetComponent<Button>().enabled = false;
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ ENTER ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    // call with Enter Button
+    public void Enter()
+    {
+        // Only if something is selected
+        for (int i = 0; i < toggleOptions.Length; i++)
+        {
+            if (toggleOptions[i].isOn)
+            {
+                totalOptions[i]++;
+                textTotalOptions[i].text = totalOptions[i].ToString();
+                options[i] = totalOptions[i];
+                Total();
+            }
+        }
+        Pie();
+        Reset();
+        timeOut = false;
+        Thanks();
+        TotalVisiteur();
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ TOTAL ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    // Each time Enter is pressed x each option
+    public void Total()
+    {
+        total++;
+        totalText.text = total.ToString();
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ VISITORS ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    //each time Enter is pressed
+    private void TotalVisiteur()
+    {
+        totalVisiteurs++;
+        totalVisiteursText.text = totalVisiteurs.ToString();
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ PIE ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    // Make the pie
+    void Pie()
+    {
+        float zRotation = 0F;
+        float totalPie = 0F;
+        if (total != 0)
+        {
+            for (int i = 0; i < options.Length; i++)
+            {
+                totalPie += options[i];
+            }
+            for (int i = 0; i < options.Length; i++)
+            {
+                Image newPie = Instantiate(PiePrefab) as Image;
+                newPie.transform.SetParent(transform, false);
+                newPie.color = PieColors[i];
+                newPie.fillAmount = options[i] / totalPie;
+                newPie.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, zRotation));
+                zRotation -= newPie.fillAmount * 360f;
+            }
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ TIME ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    // call with the Option buttons
+    public void TimeOut()
+    {
+        timeLeft = TheTimeLeft;
+        timeOut = true;
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ THANKS ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    // call thanks page
+    public void Thanks()
+    {
+        thanks.GetComponent<Image>().enabled = true;
+        thanksTimeLeft = theThanksTimeLeft;
+        thanksTime = true;
+    }
+    //----------------------------------------------------------------------------------------------------------
+    //------------------------------------------------ RESET ---------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    // Reset the options if no ones press enter
+    private void Reset()
+    {
+        //Debug.Log("Reset time !");
+        for (int i = 0; i < toggleOptions.Length; i++)
+        {
+            toggleOptions[i].isOn = false;
+            //Debug.Log((toggleOptions[i]) + " = false");
+        }
+	}
+ }
