84f7a9cc71a9273cd9c79c8db091c87b318abf06
Alex lechot <admin@uxmilk.co>
2019-02-08 04:12:29 +0100

    Q4 ... issues with my special toggle group

---
+++ Survey/Assets/Script/ToggleGroup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleGroup : MonoBehaviour
+{
+    // Here put only the Toggle you whant to switch off when the special toggle is selected 
+    public Toggle[] toggleOptions;
+    public Toggle MySecialToggle; 
+
+    // Call by my special toggle
+    public void MyToggleGroup()
+    {
+        //All the other toggle will be false when the ( Toggle 1 - where this function is called ) is selected
+        for (int i = 0; i < toggleOptions.Length; i++)
+        {
+            // all the other Toggle is false
+            toggleOptions[i].isOn = false;
+            //Debug.Log((toggleOptions[i]) + " = false")
+        }
+    }
+
+    // Call by all the other Toggle
+    public void TheOthersToggle()
+    {
+        // My special Toggle is false
+        MySecialToggle.isOn = false;
+    }
+}
